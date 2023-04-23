b657e5f263af553766ad956a3627adc1e3ac03c8
rafalebre <rafaellebre100@gmail.com>
2023-04-23 12:00:19 +0000

    Fetch + styles added

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,34 +1,60 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const List = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState ([]);
-	 
-	return (
-		<div className="container">
-			<h1>To-Do List</h1>
-			<ul>
-				<li>
-					<input 
-					type="text" 
-					onChange={(e) => setInputValue(e.target.value)}
-					value={inputValue}
-					onKeyDown={(e) => e.key === "Enter" ? setTodos(todos.concat(inputValue)) (setInputValue("")) : null}								
-					placeholder="Add a task"></input>
-					</li>
-					{todos.map((item, index) => (
-						<li>
-							 {item}{""} 
-							 <i className="fas fa-trash"  onClick={() => 
-								setTodos(todos.filter((t, currentIndex) =>	index != currentIndex))
-								}></i>
-								</li>
-					))}
-							
-			</ul>
-			<div><p>{todos.length} Jobs to do</p></div>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/rafalebre", {
+      method: "PUT",
+      body: JSON.stringify(todos),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }, [todos]);
+
+  const handleKeyDown = e => {
+    if (e.key === "Enter") {
+      const newTodo = { label: inputValue, done: false };
+      setTodos([...todos, newTodo]);
+      setInputValue("");
+    }
+  };
+
+  const handleDelete = index => {
+    const newTodos = todos.filter((todo, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="container">
+      <h1>To-Do List</h1>
+      <ul>
+        <li>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={e => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            placeholder="Add a task"
+          />
+        </li>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.label}{" "}
+            <i className="fas fa-trash" onClick={() => handleDelete(index)}></i>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <p>{todos.length} Jobs to do</p>
+      </div>
+    </div>
+  );
 };
 
 export default List;
