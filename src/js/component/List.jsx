aef83f7c88a86415fe64d154964d642e0b6c1edd
rafalebre <rafaellebre100@gmail.com>
2023-04-23 12:49:51 +0000

    Delete all and post method added; all working.

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
+import "./List.css";
 
 const List = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/rafalebre", {
+      method: "POST",
+      body: JSON.stringify([]),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Error");
+        }
+      })
+      .catch(error => {
+        console.error("There was a problem creating the list:", error);
+      });
+
     fetch("https://assets.breatheco.de/apis/fake/todos/user/rafalebre", {
       method: "PUT",
       body: JSON.stringify(todos),
@@ -30,6 +47,28 @@ const List = () => {
     setTodos(newTodos);
   };
 
+  const handleMouseEnter = index => {
+    const listItems = document.querySelectorAll(".list-item");
+    listItems[index].classList.add("hovered");
+  };
+
+  const handleMouseLeave = index => {
+    const listItems = document.querySelectorAll(".list-item");
+    listItems[index].classList.remove("hovered");
+  };
+
+  const handleClearAll = () => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/rafalebre", {
+      method: "DELETE"
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setTodos([]);
+      })
+      .catch(error => console.error(error));
+  };
+
   return (
     <div className="container">
       <h1>To-Do List</h1>
@@ -44,14 +83,33 @@ const List = () => {
           />
         </li>
         {todos.map((todo, index) => (
-          <li key={index}>
+          <li
+            key={index}
+            className="list-item"
+            onMouseEnter={() => handleMouseEnter(index)}
+            onMouseLeave={() => handleMouseLeave(index)}
+          >
             {todo.label}{" "}
-            <i className="fas fa-trash" onClick={() => handleDelete(index)}></i>
+            {index !== -1 && (
+              <i
+                className="fas fa-trash"
+                onClick={() => handleDelete(index)}
+              ></i>
+            )}
           </li>
         ))}
       </ul>
       <div>
-        <p>{todos.length} Jobs to do</p>
+        <p>
+          {!todos.length ? (
+            <h5>No tasks, add a task</h5>
+          ) : (
+            todos.length + " jobs to do"
+          )}
+        </p>
+        <button className="clear-all-btn" onClick={handleClearAll}>
+          Clear all tasks
+        </button>
       </div>
     </div>
   );
